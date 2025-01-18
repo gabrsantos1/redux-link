62c52bc9c3b803c567a28321d10c9a66623be5a9
Gabriel Santos <gs2625020@gmail.com>
2025-01-17 22:36:03 -0300

    landing page structure finished // working on styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#e7720b",
+          50: "#f5933d",
+          20: "#e6a66e",
+        },
+      },
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+        varela: ["Varela Round", "serif"],
+      },
+    },
   },
-  plugins: [],
-}
 
+  plugins: [],
+};
